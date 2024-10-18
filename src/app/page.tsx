21728407803d93a1cbf7f045e18d6dfebf382807
ch3mson <bensonyan778@hotmail.com>
2024-10-18 00:27:25 -0400

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Hero from "@/components/hero";
 import FighterInput from "@/components/compare";
 
-async function getLatestDate() {
-  return new Date().toISOString().split('T')[0];
-}
+
 
 export default async function Home() {
-  const latestDate = await getLatestDate();
 
   return (
     <main className="flex flex-col items-center justify-start px-8 py-4 sm:px-12 sm:py-6 md:px-16 md:py-8 min-h-[calc(100vh-200px)]">
-      <Hero latestDate={latestDate} />
+      <Hero />
       <FighterInput />
     </main>
   );
